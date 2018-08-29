160f1ac87990efc2a5409dfee97232962fcbd1e1
Giorgio Sironi <g.sironi@elifesciences.org>
2018-08-29 10:35:11 +1000

    Remove aws_* variables as they can be overridden with real environment variables if needed

--- tf/variables.tf
+++ tf/variables.tf
@@ -18,16 +18,6 @@ variable "account_id" {
   description = "AWS account ID."
 }
 
-variable "aws_access_key" {
-  # set using command time e.g. terraform apply -var 'access_key=foo'
-  default = "YOUR_ADMIN_ACCESS_KEY"
-}
-
-variable "aws_secret_key" {
-  # set using command time e.g. terraform apply -var 'access_key=foo'
-  default = "YOUR_ADMIN_SECRET_KEY"
-}
-
 variable "vpc_id" {
   default = "vpc-e121f79b"
   description = "Default VPC for us-east-1 on Libero account"
