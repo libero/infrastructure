de4b51e60d8ac3f09a2e81045d7ada2692de4f81
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-05-29 10:25:08 +0100

    Add S3 buckets for the content stores (#16)
    * Add S3 buckets for the content stores
    
    * CS fixes

---
+++ tf/blog-articles.tf
@@ -0,0 +1,93 @@
+resource "aws_s3_bucket" "blog_articles_assets" {
+  bucket = "${var.env}-blog-articles-assets"
+
+  tags = {
+    Environment = "${var.env}"
+  }
+}
+
+resource "aws_s3_bucket_policy" "blog_articles_assets" {
+  bucket = "${aws_s3_bucket.blog_articles_assets.id}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.blog_articles_assets.id}/*"
+            ],
+            "Effect": "Allow",
+            "Principal": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_user" "blog_articles" {
+  name = "${var.env}-blog-articles"
+  path = "/applications/"
+}
+
+resource "aws_iam_access_key" "blog_articles" {
+  user    = "${aws_iam_user.blog_articles.name}"
+  pgp_key = "${file("libero-admin.pub")}"
+}
+
+output "credentials_blog_articles_id" {
+  value = "${aws_iam_access_key.blog_articles.id}"
+}
+
+output "credentials_blog_articles_secret" {
+  value = "${aws_iam_access_key.blog_articles.encrypted_secret}"
+}
+
+resource "aws_iam_policy" "blog_articles_s3_write" {
+  name        = "blogArticlesS3Write"
+  path        = "/"
+  description = "Allows read and write access to blog-articles-assets S3 storage"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListAllMyBuckets"
+            ],
+            "Resource": ["*"]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.blog_articles_assets.id}"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:DeleteObject",
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.blog_articles_assets.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "blog_articles_s3_write" {
+  user       = "${aws_iam_user.blog_articles.name}"
+  policy_arn = "${aws_iam_policy.blog_articles_s3_write.arn}"
+}
---
+++ tf/scholarly-articles.tf
@@ -0,0 +1,93 @@
+resource "aws_s3_bucket" "scholarly_articles_assets" {
+  bucket = "${var.env}-scholarly-articles-assets"
+
+  tags = {
+    Environment = "${var.env}"
+  }
+}
+
+resource "aws_s3_bucket_policy" "scholarly_articles_assets" {
+  bucket = "${aws_s3_bucket.scholarly_articles_assets.id}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.scholarly_articles_assets.id}/*"
+            ],
+            "Effect": "Allow",
+            "Principal": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_user" "scholarly_articles" {
+  name = "${var.env}-scholarly-articles"
+  path = "/applications/"
+}
+
+resource "aws_iam_access_key" "scholarly_articles" {
+  user    = "${aws_iam_user.scholarly_articles.name}"
+  pgp_key = "${file("libero-admin.pub")}"
+}
+
+output "credentials_scholarly_articles_id" {
+  value = "${aws_iam_access_key.scholarly_articles.id}"
+}
+
+output "credentials_scholarly_articles_secret" {
+  value = "${aws_iam_access_key.scholarly_articles.encrypted_secret}"
+}
+
+resource "aws_iam_policy" "scholarly_articles_s3_write" {
+  name        = "ScholarlyArticlesS3Write"
+  path        = "/"
+  description = "Allows read and write access to scholarly-articles-assets S3 storage"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListAllMyBuckets"
+            ],
+            "Resource": ["*"]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.scholarly_articles_assets.id}"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:DeleteObject",
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.scholarly_articles_assets.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "scholarly_articles_s3_write" {
+  user       = "${aws_iam_user.scholarly_articles.name}"
+  policy_arn = "${aws_iam_policy.scholarly_articles_s3_write.arn}"
+}
