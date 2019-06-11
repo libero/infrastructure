df0b0b8bfd81e1d8a8a40ffe1898f594cea57c8c
Giorgio Sironi <g.sironi@elifesciences.org>
2019-06-11 14:14:06 +0100

    Add DNS entry for jats-ingester proxy

--- tf/main.tf
+++ tf/main.tf
@@ -135,3 +135,11 @@ resource "aws_route53_record" "api_gateway" {
   ttl     = "60"
   records = ["${aws_instance.single_node.public_ip}"]
 }
+
+resource "aws_route53_record" "jats_ingester" {
+  zone_id = "${data.aws_route53_zone.main.zone_id}"
+  name    = "${var.env}--jats-ingester.${data.aws_route53_zone.main.name}"
+  type    = "A"
+  ttl     = "60"
+  records = ["${aws_instance.single_node.public_ip}"]
+}
