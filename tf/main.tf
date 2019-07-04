4bf5677b27951383ad94bd1673713d8d5f92cab3
Giorgio Sironi <g.sironi@elifesciences.org>
2019-07-04 11:42:50 +0100

    Integrate ansible with local provisioner

--- tf/main.tf
+++ tf/main.tf
@@ -21,14 +21,8 @@ resource "aws_instance" "single_node" {
     Name = "single-node--${var.env}"
   }
 
-  provisioner "remote-exec" {
-    script = "install-docker.sh"
-
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = "${file("single-node--${var.env}.key")}"
-    }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${var.env}.${data.aws_route53_zone.main.name}, --key-file ${data.local_file.public_key.content} playbooks/*.playbook"
   }
 }
 
