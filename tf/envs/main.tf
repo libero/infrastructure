3a041590bd6bb3c3c1533ed1688b3c7cb3450f51
Giorgio Sironi <g.sironi@elifesciences.org>
2019-12-11 14:08:28 +0000

    Move main.tf out of environment-specific folder

---
+++ tf/envs/main.tf
@@ -0,0 +1,32 @@
+variable "region" {
+  default = "us-east-1"
+  description = "The AWS region."
+}
+
+variable "env" {
+  description = "Environment: unstable, demo, ..."
+}
+
+variable "vpc_id" {
+  default = "vpc-e121f79b"
+  description = "Default VPC for us-east-1 on Libero account"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "libero-terraform"
+    # specify with terraform init --backend-config="key=<env>/terraform.tfstate" to make it dynamic
+    #key    = "unstable/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+module "kubernetes_cluster" {
+  source = "../../modules/kubernetes_cluster"
+  vpc_id = var.vpc_id
+  name = "${var.env}--kubernetes-test" 
+}
