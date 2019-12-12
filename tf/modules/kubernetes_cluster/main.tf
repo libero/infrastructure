92bd4bbe845e2e653a5eebf23c9e3f0519c7c7b6
Giorgio Sironi <g.sironi@elifesciences.org>
2019-12-12 13:42:47 +0000

    Switch Terraform EKS module to unreleased 8.x
    Avoids the need for installing `kubectl` and `aws-iam-authenticator`

--- tf/modules/kubernetes_cluster/main.tf
+++ tf/modules/kubernetes_cluster/main.tf
@@ -28,7 +28,13 @@ resource "aws_security_group" "node_port_services_public_access" {
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
+  # master, unreleased 8.x
+  source = "git@github.com:terraform-aws-modules/terraform-aws-eks.git?ref=11d8ee8631ec2bb98d85295d814a4dc738026704"
+  # TODO: transition to the following
+  # when a 8.x release is available
+  #source  = "terraform-aws-modules/eks/aws"
+  #version = "7.0.0"
+
   cluster_name = var.cluster_name
   vpc_id = var.vpc_id
   subnets      = var.subnets
@@ -44,10 +50,6 @@ module "eks" {
 
   map_users                            = var.map_users
 
-  # TODO: remove as obsolete when upgrading to 8.x
-  # https://github.com/terraform-aws-modules/terraform-aws-eks/blob/11d8ee8631ec2bb98d85295d814a4dc738026704/CHANGELOG.md#v8---2019--
-  write_aws_auth_config = false
-  # end TODO
   write_kubeconfig = false
 }
 
