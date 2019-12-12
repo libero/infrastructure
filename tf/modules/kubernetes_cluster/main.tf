f55f6289ab3c9c3130ffb30cfc3d071c26ac1200
Giorgio Sironi <g.sironi@elifesciences.org>
2019-12-12 09:27:54 +0000

    Remove worker management security group
    No need for SSH access to nodes

--- tf/modules/kubernetes_cluster/main.tf
+++ tf/modules/kubernetes_cluster/main.tf
@@ -12,23 +12,6 @@ variable "vpc_id" {
 variable "subnets" {
 }
 
-resource "aws_security_group" "all_worker_mgmt" {
-  name_prefix = "all_worker_management"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-      "172.16.0.0/12",
-      "192.168.0.0/16",
-    ]
-  }
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   cluster_name = var.cluster_name
@@ -40,7 +23,6 @@ module "eks" {
       name                          = "${var.cluster_name}--small"
       instance_type                 = "t2.small"
       asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
     },
   ]
 
