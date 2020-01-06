cb8beee070f25366e1e4f92e863a09c9c70df96b
Giorgio Sironi <g.sironi@elifesciences.org>
2020-01-06 14:41:09 +0000

    Correct argument number check

--- scripts/terraform-apply.sh
+++ scripts/terraform-apply.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -lt 1 ]; then
     echo "Usage: scripts/terraform-apply.sh ENVIRONMENT"
     echo "Example: scripts/terraform-apply.sh franklin"
     exit 1
