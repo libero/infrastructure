d42bf8fb1fb79c8f5807a786a0d413c8eb99ef9e
Will Byrne <w.byrne@elifesciences.org>
2020-10-23 13:25:22 +0100

    Update to latest version of ingress-nginx chart

--- tf/modules/kubernetes_ingress/main.tf
+++ tf/modules/kubernetes_ingress/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "ingress_nginx" {
   name = "ingress-nginx"
   chart = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
-  version = "2.16.0"
+  version = "3.7.1"
 
   set {
     name  = "controller.replicaCount"
