229022d94ea7dd4ebb6c47afba37c340e28153aa
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-04 15:17:46 +0000

    add quotes around nginx log variables following example in doc sciety/sciety#342

--- tf/modules/kubernetes_ingress/main.tf
+++ tf/modules/kubernetes_ingress/main.tf
@@ -29,7 +29,7 @@ resource "helm_release" "ingress_nginx" {
 
   set {
     name  = "controller.config.log-format-upstream"
-    value = "{'time_local': $time_local, 'remote_addr': $remote_addr, 'request': $request, 'http_referrer': $http_referer, 'http_user_agent': $http_user_agent, 'status': $status, 'proxy_upstream_name': $proxy_upstream_name, 'upstream_status': $upstream_status', 'ingress_name': $ingress_name, 'namespace': $namespace}"
+    value = "{'time_local': '$time_local', 'remote_addr': '$remote_addr', 'request': '$request', 'http_referrer': '$http_referer', 'http_user_agent': '$http_user_agent', 'status': '$status', 'proxy_upstream_name': '$proxy_upstream_name', 'upstream_status': '$upstream_status', 'ingress_name': '$ingress_name', 'namespace': '$namespace'}"
     type  = "string"
   }
 }
