ce4dce36f1e47c0cf359bc9a1f8eb1fa7fd4b71e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-04 17:25:04 +0000

    Use values.yaml instead of 'set' for k8s ingress helm_release sciety/sciety#342

--- tf/modules/kubernetes_ingress/main.tf
+++ tf/modules/kubernetes_ingress/main.tf
@@ -4,32 +4,7 @@ resource "helm_release" "ingress_nginx" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   version = "3.7.1"
 
-  set {
-    name  = "controller.replicaCount"
-    value = 2
-  }
-
-  set {
-    name  = "controller.config.use-gzip"
-    value = "true"
-    type  = "string"
-  }
-
-  set {
-    name  = "controller.config.enable-brotli"
-    value = "true"
-    type  = "string"
-  }
-
-  set {
-    name  = "controller.config.log-format-escape-json"
-    value = "true"
-    type  = "string"
-  }
-
-  set {
-    name  = "controller.config.log-format-upstream"
-    value = "${jsonencode(jsondecode(file("${path.module}/nginx-log-format-upstream.json")))}"
-    type  = "string"
-  }
+  values = [
+    "${file("${path.module}/values.yaml")}"
+  ]
 }
