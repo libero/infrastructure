0f9a3e716191f61aaa2c540e4413f408a002ae17
Giorgio Sironi <g.sironi@elifesciences.org>
2020-12-07 15:49:54 +0000

    Create new ingress-controller for NLB usage sciety/sciety#344

--- tf/modules/kubernetes_ingress/main.tf
+++ tf/modules/kubernetes_ingress/main.tf
@@ -8,3 +8,14 @@ resource "helm_release" "ingress_nginx" {
     "${file("${path.module}/values.yaml")}"
   ]
 }
+
+resource "helm_release" "ingress_nginx_nlb" {
+  name = "ingress-nginx-nlb"
+  chart = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  version = "3.7.1"
+
+  values = [
+    "${file("${path.module}/values-nlb.yaml")}"
+  ]
+}
